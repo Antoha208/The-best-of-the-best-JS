0e9dfc0d8765eb3d99cf3a01d5d0b962cedaba97
Antoha208 <anton_androsyak@mail.ru>
2021-02-21 15:41:36 +0300

    console

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
-const levels = document.querySelectorAll("li.main-text-list__item").forEach(level => {
-	level.addEventListener("click", onClick)
+const levels = document.querySelector(".main-text-list");
+console.log(levels)
+levels.addEventListener('click', event => console.log(event))
+
+
+/*levels.forEach(level => {
+	level.addEventListener("click", setLevel)
 })
 
-function onClick(level) {
-  document.getElementById("easy").classList.remove(".main-text-list__item-active");
-  document.getElementById("medium").classList.remove(".main-text-list__item-active")
-  document.getElementById("hard").classList.remove(".main-text-list__item-active")
-  level.target.classList.add(".main-text-list__item-active")
+function setLevel(level) {
+  levels = +e.target.getAttribute("data-value")
+  document.getElementById("easy").classList.remove("main-text-list__item-active");
+  document.getElementById("medium").classList.remove("main-text-list__item-active")
+  document.getElementById("hard").classList.remove("main-text-list__item-active")
+  level.target.classList.add("main-text-list__item-active")
 }
 
 
-/*let selection = selectLevel () => {
-levels.forEach((levels) => (level, index, levels) {
-  if (level.className === ('main-text-list__item')) {
-    level.classList.add('main-text-list__item-active')
-  } else {
-    level.classList.remove('main-text-list__item-active')
+const startGame = document.querySelector(".main-text-button").onClick () => createElement('.body');
+console.log(startGame)
+startGame.addEventListener("click", selectedLevel)
+
+let background = document.createElement('div');
+  background.url = ('../img/Background.png')
+  background.style.backgroundSize = '100%'
+  background.style.backgroundPosition = 'cover'
+  document.body.append(background)
+  console.log(background)
+
+function selectedLevel () {
+  switch (levels) {
+    case "id = easy":
+    const cardsEasy = document.createElement('div');
+    cardsEasy.url = ('../img/Карта 1.png');
+    document.body.append(background);
+      break;
+    case "id = medium":
+    const cardsMedium = document.createElement('div');
+    cardsEasy.url = ('../img/Карта 1.png');
+    document.body.append(background);
+      break;
+    case "id = hard":
+    const cardsHard = document.createElement('div');
+    cardsEasy.url = ('../img/Карта 1.png');
+    document.body.append(background);
+    default: ('');
   }
-    });
-}
-
-function addClass () {
-return levels.addEventListener('click', event => selection)
-}
-addClass()*/
+}*/
